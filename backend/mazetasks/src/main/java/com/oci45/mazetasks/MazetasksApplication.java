cae96683ff0d14180904f4888c1c68caa515588f
emiduran05 <emiduranf05@gmail.com>
2026-04-16 14:06:48 -0600

    backend created + initial database config

---
+++ backend/mazetasks/src/main/java/com/oci45/mazetasks/MazetasksApplication.java
@@ -0,0 +1,13 @@
+package com.oci45.mazetasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazetasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazetasksApplication.class, args);
+	}
+
+}
---
+++ backend/mazetasks/src/test/java/com/oci45/mazetasks/MazetasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oci45.mazetasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazetasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
