f311523e1253caf2cfd02d0309fd0ca6ee369bc5
JuanAndresA016 <a01644650@tec.mx>
2026-05-20 23:05:50 +0000

    Fix: add docker login step

--- backend/mazetasks/build.sh
+++ backend/mazetasks/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-
 export IMAGE_NAME=mazetasks
 export IMAGE_VERSION=0.1
-
-if [ -z "$DOCKER_REGISTRY" ]; then
-    export DOCKER_REGISTRY=$(state_get DOCKER_REGISTRY)
-    echo "DOCKER_REGISTRY set."
-fi
 
 if [ -z "$DOCKER_REGISTRY" ]; then
     echo "Error: DOCKER_REGISTRY env variable needs to be set!"
@@ -14,7 +8,6 @@ if [ -z "$DOCKER_REGISTRY" ]; then
 fi
 
 export IMAGE=${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}
-
 mvn clean package spring-boot:repackage
 docker build -f Dockerfile -t $IMAGE .
 docker push $IMAGE
